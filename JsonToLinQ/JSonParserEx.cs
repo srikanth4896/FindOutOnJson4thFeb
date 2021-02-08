5b1ca8e32c444e572ff71d59ebb2407a444be4a3
Srikanth <srikanth.chilamkuri@zensar.com>
2021-02-08 08:29:03 +0530

    Json file Creation Added

---
+++ JsonToLinQ/JSonParserEx.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonToLinQ
+{
+    class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Degree { get; set; }
+        public List<string> Hobbies { get; set; }
+        public override string ToString()
+        {
+            return string.Format($"{Id},{Name},{Degree},{Hobbies}");
+        }
+
+    }
+
+    class JSonParserEx
+    {
+        static void Main()
+        {
+            Student student = new Student() 
+            { 
+                Id=1,
+                Name="Srikanth",
+                Degree ="BTech",
+                Hobbies=new List<string>
+                {
+                    "Reading",
+                    "Playing Games"
+                }
+            };
+           string s=JsonConvert.SerializeObject(student);
+            //Console.WriteLine(s);
+            File.WriteAllText(@"student.json", s);
+            Console.WriteLine("Stored");
+
+            string ds = String.Empty;
+            ds = File.ReadAllText(@"student.json");
+          Student rs=  JsonConvert.DeserializeObject<Student>(ds);
+            Console.WriteLine(rs);
+        }
+    }
+}
---
+++ JsonToLinQ/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace JsonToLinQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            StreamReader reader = File.OpenText(@"F:\DotNet 2021\ConsoleAssignments\JsonToLinQ\JsonToLinQ\example_2.json");
+
+            JObject o = (JObject)JToken.ReadFrom(new JsonTextReader(reader));
+
+            string question = (string)o["quiz"]["sport"]["q1"]["question"];
+            Console.WriteLine(question);
+    JToken j=     o["quiz"]["sport"]["q1"]["options"];
+            Console.WriteLine(j);
+            IList<string> anwers = j.Select(c => (string)c).ToList();
+            foreach (var i in anwers)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("------------------");
+            JToken j1 = o["quiz"];
+         var i1=   j.Select(c => (string)c).Select(c => (String)c);
+            foreach (var item in i1)
+            {
+                Console.WriteLine(item);
+            }
+
+            JToken j2 = o["quiz"]["maths"];
+            var i2 = j2.Select(c => c);
+            foreach (var item in i2)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("---------");
+            var post = from p in o["quiz"]["maths"]["q1"].SelectMany(i => i).Values()
+                       select p;
+            foreach (var item in post)
+            {
+                Console.WriteLine(item);
+            }
+            var post1 = from p in o["quiz"]["sport"]["q1"].Children()
+                       select p;
+            foreach (var item in post1)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("-----------------");
+            var post2 = from p in o["quiz"]["sport"]["q1"].Ancestors().Last()
+                        select p;
+            foreach (var item in post2)
+            {
+                Console.WriteLine(item);
+            }
+
+
+        }
+    }
+}
